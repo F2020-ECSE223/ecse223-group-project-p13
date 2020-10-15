0bbc6b9185e1dad398dcfa92242ad8bd1952b188
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2020-10-15 16:48:37 -0400

    updated from common branch

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/application/FlexiBookApplication.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/application/FlexiBookApplication.java
@@ -3,12 +3,22 @@
  */
 package ca.mcgill.ecse.flexibook.application;
 
-public class FlexiBookApplication {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import ca.mcgill.ecse.flexibook.model.FlexiBook;
 
+public class FlexiBookApplication {
+	private static FlexiBook flexibook ;
+	
     public static void main(String[] args) {
         System.out.println(new FlexiBookApplication().getGreeting());
     }
+    	
+    	
+    public static FlexiBook getFlexiBook() {
+    	if (flexibook == null) {
+    		// for now, we are just creating an empty BTMS
+    		flexibook = new flexibook();
+    	}
+     	return flexibook;
+    }
+    	
 }
--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
@@ -5,4 +5,5 @@ public class FlexiBookController {
 	public FlexiBookController() {
 		
 	}
+	public static void 
 }
