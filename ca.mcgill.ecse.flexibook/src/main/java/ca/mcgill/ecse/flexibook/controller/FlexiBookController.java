25914e2067ae38d9f3e1a8628f4aae72b93a9bbf
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2020-10-15 18:19:20 -0400

    added cancel method to controller

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/controller/FlexiBookController.java
@@ -1,5 +1,9 @@
 package ca.mcgill.ecse.flexibook.controller;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import ca.mcgill.ecse.flexibook.application.FlexiBookApplication;
 import ca.mcgill.ecse.flexibook.model.Appointment;
@@ -21,6 +25,46 @@ public class FlexiBookController {
 	public static void makeAppointment(){
 		FlexiBook flexibook = FlexiBookApplication.getFlexiBook();
 	}
-	public static void cancelAppointment(){}
+	public static void cancelAppointment(User customer, Appointment appointment) throws InvalidInputException {
+		FlexiBook flexibook = FlexiBookApplication.getFlexiBook();
+		try{
+			if(customer.equals(appointment.getCustomer())){
+				throw new InvalidInputException("A customer can only cancel their own appointments");
+			}
+			if(customer.equals(flexibook.getOwner())){
+				throw new InvalidInputException("An owner cannot cancel an appointment");
+			}
+			if(appointment.getTimeSlot().getStartDate().compareTo(getCurrentDate()) >= 0){
+				throw new InvalidInputException("Cannot cancel an appointment on the appointment date");
+			}
+			else{
+				appointment.delete();
+			}
+		}
+		catch (RuntimeException e){
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
 	public static void updateAppointment(){}
+
+	private static Date cleanDate(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(date.getTime());
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		java.util.Date tempCleanedDate = cal.getTime();
+		java.sql.Date cleanedDate = new java.sql.Date(tempCleanedDate.getTime());
+		return cleanedDate;
+	}
+	private static java.util.Date getCurrentDate(){
+		java.util.Calendar cal = java.util.Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		java.util.Date date = cal.getTime();
+		return date;
+	}
 }
