627ce1a0a25e5263cb6e0ae83484e6037aa15b0a
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2020-10-18 14:12:24 -0400

    added all step def methods

--- ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse.flexibook/src/test/java/ca/mcgill/ecse/flexibook/features/CucumberStepDefinitions.java
@@ -397,4 +397,257 @@ public class CucumberStepDefinitions {
     @When("{string} attempts to update {string}'s {string} appointment on {string} at {string} to {string} at {string}")
     public void attemptsToUpdateSAppointmentOnAtToAt(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) {
     }
+
+    @When("{string} initiates the addition of the service {string} with duration {string}, start of down time {string} and down time duration {string}")
+    public void initiatesTheAdditionOfTheServiceWithDurationStartOfDownTimeAndDownTimeDuration(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("the service {string} shall exist in the system")
+    public void theServiceShallExistInTheSystem(String arg0) {
+    }
+
+    @Then("the service {string} shall have duration {string}, start of down time {string} and down time duration {string}")
+    public void theServiceShallHaveDurationStartOfDownTimeAndDownTimeDuration(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @Then("the number of services in the system shall be {string}")
+    public void theNumberOfServicesInTheSystemShallBe(String arg0) {
+    }
+
+    @Then("the service {string} shall not exist in the system")
+    public void theServiceShallNotExistInTheSystem(String arg0) {
+    }
+
+    @Then("the service {string} shall still preserve the following properties:")
+    public void theServiceShallStillPreserveTheFollowingProperties(String arg0) {
+    }
+
+    @Given("an owner account exists in the system with username {string} and password {string}")
+    public void anOwnerAccountExistsInTheSystemWithUsernameAndPassword(String arg0, String arg1) {
+    }
+
+    @Given("the account with username {string} has pending appointments")
+    public void theAccountWithUsernameHasPendingAppointments(String arg0) {
+    }
+
+    @Given("the user is logged in to an account with username {string}")
+    public void theUserIsLoggedInToAnAccountWithUsername(String arg0) {
+    }
+
+    @When("the user tries to delete account with the username {string}")
+    public void theUserTriesToDeleteAccountWithTheUsername(String arg0) {
+    }
+
+    @Then("the account with the username {string} does not exist")
+    public void theAccountWithTheUsernameDoesNotExist(String arg0) {
+    }
+
+    @Then("all associated appointments of the account with the username {string} shall not exist")
+    public void allAssociatedAppointmentsOfTheAccountWithTheUsernameShallNotExist(String arg0) {
+    }
+
+    @Then("the user shall be logged out")
+    public void theUserShallBeLoggedOut() {
+    }
+
+    @Then("the account with the username {string} exists")
+    public void theAccountWithTheUsernameExists(String arg0) {
+    }
+
+    @Then("an error message {string} shall be raised")
+    public void anErrorMessageShallBeRaised(String arg0) {
+    }
+
+    @When("{string} initiates the deletion of service {string}")
+    public void initiatesTheDeletionOfService(String arg0, String arg1) {
+    }
+
+    @Then("the service combos {string} shall not exist in the system")
+    public void theServiceCombosShallNotExistInTheSystem(String arg0) {
+    }
+
+    @Then("the service combos {string} shall not contain service {string}")
+    public void theServiceCombosShallNotContainService(String arg0, String arg1) {
+    }
+
+    @When("the user tries to log in with username {string} and password {string}")
+    public void theUserTriesToLogInWithUsernameAndPassword(String arg0, String arg1) {
+    }
+
+    @Then("the user should be successfully logged in")
+    public void theUserShouldBeSuccessfullyLoggedIn() {
+    }
+
+    @Then("the user should not be logged in")
+    public void theUserShouldNotBeLoggedIn() {
+    }
+
+    @Then("a new account shall be created")
+    public void aNewAccountShallBeCreated() {
+    }
+
+    @Then("the account shall have username {string} and password {string}")
+    public void theAccountShallHaveUsernameAndPassword(String arg0, String arg1) {
+    }
+
+    @Then("the user shall be successfully logged in")
+    public void theUserShallBeSuccessfullyLoggedIn() {
+    }
+
+    @Given("the user is logged out")
+    public void theUserIsLoggedOut() {
+    }
+
+    @When("the user tries to log out")
+    public void theUserTriesToLogOut() {
+    }
+
+    @Given("no business exists")
+    public void noBusinessExists() {
+    }
+
+    @When("the user tries to set up the business information with new {string} and {string} and {string} and {string}")
+    public void theUserTriesToSetUpTheBusinessInformationWithNewAndAndAnd(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @Then("a new business with new {string} and {string} and {string} and {string} shall {string} created")
+    public void aNewBusinessWithNewAndAndAndShallCreated(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("an error message {string} shall {string} raised")
+    public void anErrorMessageShallRaised(String arg0, String arg1) {
+    }
+
+    @Given("a business exists with the following information:")
+    public void aBusinessExistsWithTheFollowingInformation() {
+    }
+
+    @Given("the business has a business hour on {string} with start time {string} and end time {string}")
+    public void theBusinessHasABusinessHourOnWithStartTimeAndEndTime(String arg0, String arg1, String arg2) {
+    }
+
+    @When("the user tries to add a new business hour on {string} with start time {string} and end time {string}")
+    public void theUserTriesToAddANewBusinessHourOnWithStartTimeAndEndTime(String arg0, String arg1, String arg2) {
+    }
+
+    @Then("a new business hour shall {string} created")
+    public void aNewBusinessHourShallCreated(String arg0) {
+    }
+
+    @When("the user tries to access the business information")
+    public void theUserTriesToAccessTheBusinessInformation() {
+    }
+
+    @Then("the {string} and {string} and {string} and {string} shall be provided to the user")
+    public void theAndAndAndShallBeProvidedToTheUser(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @Given("a {string} time slot exists with start time {string} at {string} and end time {string} at {string}")
+    public void aTimeSlotExistsWithStartTimeAtAndEndTimeAt(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @When("the user tries to add a new {string} with start date {string} at {string} and end date {string} at {string}")
+    public void theUserTriesToAddANewWithStartDateAtAndEndDateAt(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("a new {string} shall {string} be added with start date {string} at {string} and end date {string} at {string}")
+    public void aNewShallBeAddedWithStartDateAtAndEndDateAt(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5) {
+    }
+
+    @Given("there is no existing username {string}")
+    public void thereIsNoExistingUsername(String arg0) {
+    }
+
+    @When("the user provides a new username {string} and a password {string}")
+    public void theUserProvidesANewUsernameAndAPassword(String arg0, String arg1) {
+    }
+
+    @Then("a new customer account shall be created")
+    public void aNewCustomerAccountShallBeCreated() {
+    }
+
+    @Then("no new account shall be created")
+    public void noNewAccountShallBeCreated() {
+    }
+
+    @Given("there is an existing username {string}")
+    public void thereIsAnExistingUsername(String arg0) {
+    }
+
+    @When("the user tries to update account with a new username {string} and password {string}")
+    public void theUserTriesToUpdateAccountWithANewUsernameAndPassword(String arg0, String arg1) {
+    }
+
+    @Then("the account shall not be updated")
+    public void theAccountShallNotBeUpdated() {
+    }
+
+    @When("the user tries to update the business information with new {string} and {string} and {string} and {string}")
+    public void theUserTriesToUpdateTheBusinessInformationWithNewAndAndAnd(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @Then("the business information shall {string} updated with new {string} and {string} and {string} and {string}")
+    public void theBusinessInformationShallUpdatedWithNewAndAndAnd(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @When("the user tries to change the business hour {string} at {string} to be on {string} starting at {string} and ending at {string}")
+    public void theUserTriesToChangeTheBusinessHourAtToBeOnStartingAtAndEndingAt(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("the business hour shall {string} be updated")
+    public void theBusinessHourShallBeUpdated(String arg0) {
+    }
+
+    @When("the user tries to remove the business hour starting {string} at {string}")
+    public void theUserTriesToRemoveTheBusinessHourStartingAt(String arg0, String arg1) {
+    }
+
+    @Then("the business hour starting {string} at {string} shall {string} exist")
+    public void theBusinessHourStartingAtShallExist(String arg0, String arg1, String arg2) {
+    }
+
+    @Then("an error message {string} shall {string} be raised")
+    public void anErrorMessageShallBeRaised(String arg0, String arg1) {
+    }
+
+    @When("the user tries to change the {string} on {string} at {string} to be with start date {string} at {string} and end date {string} at {string}")
+    public void theUserTriesToChangeTheOnAtToBeWithStartDateAtAndEndDateAt(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) {
+    }
+
+    @Then("the {string} shall {string} be updated with start date {string} at {string} and end date {string} at {string}")
+    public void theShallBeUpdatedWithStartDateAtAndEndDateAt(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5) {
+    }
+
+    @When("the user tries to remove an existing {string} with start date {string} at {string} and end date {string} at {string}")
+    public void theUserTriesToRemoveAnExistingWithStartDateAtAndEndDateAt(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+    @Then("the {string} with start date {string} at {string} shall {string} exist")
+    public void theWithStartDateAtShallExist(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @When("{string} initiates the update of the service {string} to name {string}, duration {string}, start of down time {string} and down time duration {string}")
+    public void initiatesTheUpdateOfTheServiceToNameDurationStartOfDownTimeAndDownTimeDuration(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5) {
+    }
+
+    @Then("the service {string} shall be updated to name {string}, duration {string}, start of down time {string} and down time duration {string}")
+    public void theServiceShallBeUpdatedToNameDurationStartOfDownTimeAndDownTimeDuration(String arg0, String arg1, String arg2, String arg3, String arg4) {
+    }
+
+
+    @When("{string} requests the appointment calendar for the week starting on {string}")
+    public void requestsTheAppointmentCalendarForTheWeekStartingOn(String arg0, String arg1) {
+    }
+
+    @Then("the following slots shall be unavailable:")
+    public void theFollowingSlotsShallBeUnavailable() {
+    }
+
+    @Then("the following slots shall be available:")
+    public void theFollowingSlotsShallBeAvailable() {
+    }
+
+    @When("{string} requests the appointment calendar for the day of {string}")
+    public void requestsTheAppointmentCalendarForTheDayOf(String arg0, String arg1) {
+    }
 }
