d8c3170a39e6a0d83e1cffb3f2436a55a4c84674
Tomasz Mroz <tomasz.mroz@mail.mcgill.ca>
2020-11-08 16:41:43 -0500

    colors

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/FlexiBookPage.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse/flexibook/view/FlexiBookPage.java
@@ -5,5 +5,9 @@ import ca.mcgill.ecse.flexibook.controller.FlexiBookController;
 import ca.mcgill.ecse.flexibook.controller.TOAppointmentCalendarItem;
 import ca.mcgill.ecse.flexibook.controller.TOBusinessInfo;
 
+import java.awt.*;
+
 public class FlexiBookPage extends JFrame {
+    private Color[] colors = {new Color(226,240,249),new Color(176,221,228),new Color(40,111,180)
+            ,Color.WHITE,new Color(223,76,115)};
 }
